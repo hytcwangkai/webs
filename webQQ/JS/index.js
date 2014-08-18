a59c7e1f2e9e80936de300fae36c92554272ee8e
hytcwangkai <570848924@qq.com>
2014-08-18 17:19:49 +0800

    8.18下午
    webQQ继续完善

--- webQQ/JS/index.js
+++ webQQ/JS/index.js
@@ -3,7 +3,14 @@ $(function(){
 			var closeID = $(this).attr("parentid");
 			$("#" + closeID).remove();
 			$("*").find("."+closeID).attr("Isopen","1");
+			alert("f"+closeID);
+			$(".f"+closeID).remove();
 		})
+	$(document).on("click",".dian2",function(){
+		var chooseName=$(this).attr("friendName");
+		var html=chooseName;
+		$(".fName").html(html);
+	})
 	$(".pclick").click(function(){
 		var jiekou=$(this).attr("jiekou");
 		$("*").find(".panelTop").removeClass("yingc");
@@ -16,35 +23,23 @@ $(function(){
 		var friendid=$(this).attr("friendId");
 		var friendname=$(this).attr("friendName");
 		var html="";
-		html +='<div class="ltck" id="frm'+friendid+'">';
-		html +='	<div class="ltck_header">';
-		html +='		<div class="fName">'+friendname+'</div>'
-		html +='		<button class="tck_header_rbutton" parentid="frm' + friendid  + '">';
-		html +='			<span>关闭</span>';
-		html +='		</button>';
-		html +='	</div>';
-		html +='	<div class="ltck_body"></div>';
-		html +='	<div class="ltck_footer">';
-		html +='		<div class="chat">';
-		html +='			<div id="add_face_btn" class="btn_btn.face">';
-		html +='			<span class="btn_img"></span>';
-		html +='		</div>';
-		html +='		<textarea id="chat_textarea" class="input"></textarea>';
-		html +=	'		<button id="send_chat_btn" class="btn_small">';
-		html +='			<span class="btn_text">发送</span>';
-		html +='		</button>';
-		html +='		</div>';
-		html +='	</div>';
-		html +='</div>';
+		html +=friendname;
+		var choose="";
+		choose +='<li class="dian2 ffrm'+friendid+' ffrm" friendName="'+friendname+'" borethid="frm'+friendid+'" Isopen="1">';
+		choose +='	<div class="tou1">';
+		choose +='		<img src="CSS/Images/gfrm'+friendid+'.jpg">';
+		choose +='	</div>';
+		choose +='  		<p>'+friendname+'</p>'
+		choose +='</li>'
 		if(isopen==1){
-			$("body").append(html);
+			$(".fName").html(html);
+			$(".chatchoosecontent").append(choose);
 			$(".ltck").removeClass("yingc");
 			$("#frm"+friendid).addClass("yingc");
 			$(this).attr("Isopen","0");
 		}
 		else{
-			$(".ltck").removeClass("yingc");
-			$("#frm"+friendid).addClass("yingc");
+			$(".fName").html(html);
 		}
 	});
 
