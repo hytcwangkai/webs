6064906b342c8e19c8e2bc00194492825d2e705a
hytcwangkai <570848924@qq.com>
2014-08-19 11:56:20 +0800

    8.19上午
    js继续完成

--- webQQ/JS/index.js
+++ webQQ/JS/index.js
@@ -1,15 +1,31 @@
 $(function(){
+	$(document).on("click",".imgFalse",function(){
+		alert("666");
+		var closeId=$(this).attr("parentid");
+		$(".ffrm"+closeId).remove();
+	})
+	$(document).on("click","#send_chat_btn",function(){
+		var chatting_content_main="";
+		var chattingcontent=$("#chat_textarea").val();
+		var sendmainid=$(this).attr("sendId");
+		chatting_content_main+='<div class="chatting_content_main_L">'+chattingcontent+'</div>';
+		$("#fffrm"+sendmainid).append(chatting_content_main);
+		document.getElementById("chat_textarea").value="";
+
+	})
 	$(document).on("click",".tck_header_rbutton",function(){
-			var closeID = $(this).attr("parentid");
-			$("#" + closeID).remove();
-			$("*").find("."+closeID).attr("Isopen","1");
-			alert("f"+closeID);
-			$(".f"+closeID).remove();
+		$(".ltckmain").hide();
+		$(".dian2").remove();
+		$(".ltck_body_content").remove();
+		$(".dian").attr("Isopen","1");
 		})
 	$(document).on("click",".dian2",function(){
 		var chooseName=$(this).attr("friendName");
-		var html=chooseName;
-		$(".fName").html(html);
+		var chooseId=$(this).attr("borethid");
+		$("#send_chat_btn").attr("sendId",chooseId);
+		$(".fName").html(chooseName);
+		$(".ltck_body_content").hide();
+		$("#fffrm"+chooseId).show();
 	})
 	$(".pclick").click(function(){
 		var jiekou=$(this).attr("jiekou");
@@ -19,28 +35,41 @@ $(function(){
 		$(this).addClass("selected");
 	})
 	$(".dian").click(function(){
+		$(".ltckmain").show();
 		var isopen=$(this).attr("Isopen");
 		var friendid=$(this).attr("friendId");
 		var friendname=$(this).attr("friendName");
+		$("#send_chat_btn").attr("sendId",friendid)
+		var chatting="";
+		chatting+='<div class="ltck_body_content" id=fffrm'+friendid+' ></div>';
 		var html="";
 		html +=friendname;
 		var choose="";
-		choose +='<li class="dian2 ffrm'+friendid+' ffrm" friendName="'+friendname+'" borethid="frm'+friendid+'" Isopen="1">';
+		choose +='<li class="dian2 ffrm'+friendid+' ffrm" friendName="'+friendname+'" borethid="'+friendid+'" Isopen="1">';
 		choose +='	<div class="tou1">';
 		choose +='		<img src="CSS/Images/gfrm'+friendid+'.jpg">';
 		choose +='	</div>';
 		choose +='  		<p>'+friendname+'</p>'
+		choose +='   <div class="imgFalse" parentid="'+friendid+'"></div>'
 		choose +='</li>'
 		if(isopen==1){
 			$(".fName").html(html);
 			$(".chatchoosecontent").append(choose);
-			$(".ltck").removeClass("yingc");
-			$("#frm"+friendid).addClass("yingc");
+			$(".ltck_body").append(chatting);
+			$(".ltck_body_content").hide();
+			$("#fffrm"+friendid).show();
 			$(this).attr("Isopen","0");
 		}
 		else{
 			$(".fName").html(html);
+			$(".ltck_body_content").hide();
+			$("#fffrm"+friendid).show();
 		}
+		$(".dian2").hover(function(){
+			$(this).find(".imgFalse").show();
+		},function(){
+			$(this).find(".imgFalse").hide();
+		})
 	});
 
 });
